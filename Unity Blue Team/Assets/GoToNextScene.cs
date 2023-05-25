c12cd60e14cdf35bfc83abcfccd9e1403079469a
guciaq <paskiewiczkamila@gmail.com>
2023-05-25 14:57:58 +0200

    no ten tego

---
+++ Unity Blue Team/Assets/GoToNextScene.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GoToNextScene : MonoBehaviour
+{
+    void Start()
+    {
+        StartCoroutine(NextScene());
+    }
+
+    void Update()
+    {
+
+    }
+
+    IEnumerator NextScene()
+    {
+        yield return new WaitForSeconds(35.0f);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
